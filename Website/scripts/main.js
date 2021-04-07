e13b5d25f6772028bcbfafbabae1c981989192e2
Shaheer Shahzad <shaheer19962012@gmail.com>
2021-04-08 01:50:52 +0200

    Some fixes

--- Website/scripts/main.js
+++ Website/scripts/main.js
@@ -32,6 +32,7 @@ $(function () {
     }
 
     $(document).scroll(function () {
+        console.log("test")
         if ($(document).scrollTop() >= 500) {
             // user scrolled 500 pixels or more
             $("#back-top").show();
@@ -40,19 +41,19 @@ $(function () {
         }
     });
 
-    document.addEventListener("DOMContentLoaded", function(){
-        window.addEventListener('scroll', function() {
+    document.addEventListener("DOMContentLoaded", function () {
+        window.addEventListener('scroll', function () {
             if (window.scrollY > 50) {
-              document.getElementById('navbar').classList.add('fixed-top');
-              // add padding top to show content behind navbar
-              navbar_height = document.querySelector('.navbar').offsetHeight;
-              document.body.style.paddingTop = navbar_height + 'px';
+                document.getElementById('navbar').classList.add('fixed-top');
+                // add padding top to show content behind navbar
+                navbar_height = document.querySelector('.navbar').offsetHeight;
+                document.body.style.paddingTop = navbar_height + 'px';
             } else {
-              document.getElementById('navbar').classList.remove('fixed-top');
-               // remove padding top from body
-              document.body.style.paddingTop = '0';
-            } 
+                document.getElementById('navbar').classList.remove('fixed-top');
+                // remove padding top from body
+                document.body.style.paddingTop = '0';
+            }
         });
-      }); 
+    });
 
 });
